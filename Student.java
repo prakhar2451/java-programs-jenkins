7333e934af9ee7bdde3b89789131a4f57705daa1
prakhar2451 <38582421+prakhar2451@users.noreply.github.com>
2024-04-01 22:14:14 +0530

    new file added

---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    
+    public Student () {
+        System.out.println("Creating student object...");
+    }
+
+    public void Greet() {
+        System.out.println("Hello, GoodMorning!!");
+    }
+}
