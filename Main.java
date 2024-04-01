d63ab1508a33af2fe11fc4bd11b647263f4e0c30
prakhar2451 <38582421+prakhar2451@users.noreply.github.com>
2024-04-01 21:52:33 +0530

    initial commit

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("This is java program.");
         System.out.println("output from java program");
         System.out.println(new Date());
+        System.out.println("hello world!");
     }
 }
